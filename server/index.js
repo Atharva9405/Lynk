6b6741fd39962ec7018b3bd22084e7f957785cfd
Atharva <atharvanagore2004@gmail.com>
2024-10-05 22:41:58 +0530

    setup server side

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.MONGO_URL;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true,
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
