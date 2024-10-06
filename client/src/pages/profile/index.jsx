308560decc523d9bca1240dfa5fc503b06f5ae8e
Atharva <atharvanagore2004@gmail.com>
2024-10-06 11:49:18 +0530

    setup zustand

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,7 +1,11 @@
+import { useAppStore } from "@/store"
+
 const Profile = () => {
+  const {userInfo} = useAppStore();
   return (
     <div>
       Profile
+      <div>Email:{userInfo.id}</div>
     </div>
   )
 }
