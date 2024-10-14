3e1116435465bf44cc4ce8e092872a4fa3e5c06b
Atharva <atharvanagore2004@gmail.com>
2024-10-15 00:01:55 +0530

    added show uploading and downloading file progress functionality

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -9,7 +9,13 @@ import { IoSend } from "react-icons/io5";
 import { RiEmojiStickerLine } from "react-icons/ri";
 
 const MessageBar = () => {
-  const { selectedChatType, selectedChatData, userInfo } = useAppStore();
+  const {
+    selectedChatType,
+    selectedChatData,
+    userInfo,
+    setIsUploading,
+    setFileUploadProgress
+  } = useAppStore();
   const emojiRef = useRef();
   const fileInputRef = useRef();
   const socket = useSocket();
@@ -43,6 +49,7 @@ const MessageBar = () => {
         fileUrl: undefined,
       });
     }
+    setMessage("");
   };
 
   const handleAttachmentClick = () => {
@@ -57,10 +64,15 @@ const MessageBar = () => {
       if (file) {
         const formData = new FormData();
         formData.append("file", file);
+        setIsUploading(true)
         const response = await apiClient.post(UPLOAD_FILE_ROUTE, formData, {
           withCredentials: true,
+          onUploadProgress: data => {
+            setFileUploadProgress(Math.round((100 * data.loaded) / data.total));
+          }
         });
         if (response.status === 200 && response.data) {
+          setIsUploading(false)
           if (selectedChatType === "contact") {
             socket.emit("sendMessage", {
               sender: userInfo.id,
@@ -72,11 +84,13 @@ const MessageBar = () => {
           }
         }
       }
-      console.log({file});
+      console.log({ file });
     } catch (error) {
+      setIsUploading(false)
       console.log({ error });
     }
   };
+  
 
   return (
     <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6">
