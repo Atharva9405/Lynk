f0f7d42d5981ceb0685aa1617403a8b9e6590828
Atharva <atharvanagore2004@gmail.com>
2024-10-15 12:13:45 +0530

    created channel modal ui

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -5,20 +5,22 @@ import { apiClient } from "@/lib/api-client";
 import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
 import { useAppStore } from "@/store";
 import ContactList from "@/components/contact-list";
+import CreateChannel from "./components/create-channel";
 
 const ContactsContainer = () => {
-
-  const {setDirectMessagesContacts,directMessagesContacts} = useAppStore()
+  const { setDirectMessagesContacts, directMessagesContacts } = useAppStore();
 
   useEffect(() => {
     const getContacts = async () => {
-      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES,{withCredentials:true})
-      if(response.data.contacts){
-        setDirectMessagesContacts(response.data.contacts)
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES, {
+        withCredentials: true,
+      });
+      if (response.data.contacts) {
+        setDirectMessagesContacts(response.data.contacts);
       }
-    }
-    getContacts()
-  },[])
+    };
+    getContacts();
+  }, []);
 
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
@@ -37,6 +39,7 @@ const ContactsContainer = () => {
       <div className="my-5">
         <div className="flex items-start justify-between pr-10">
           <Title text="Channels" />
+          <CreateChannel />
         </div>
       </div>
       <ProfileInfo />
@@ -79,7 +82,7 @@ const Logo = () => {
   );
 };
 
-const Title = ({text}) => {
+const Title = ({ text }) => {
   return (
     <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
       {text}
